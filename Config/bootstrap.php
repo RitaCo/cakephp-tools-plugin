3a3af5408140edcf61ed836a6674cb399aa2ca8c
zoghal <saleh.souzanchi@gmail.com>
2013-11-19 06:32:40 +0330

    up

---
+++ Config/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'Browser.php';
+include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'Language.php';
+include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'OS.php';
+
+
+
+
+
