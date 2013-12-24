70fbe9a02525493f86de669ee33f030f6ea6c266
zoghal <saleh.souzanchi@gmail.com>
2013-12-24 10:01:53 +0330

    up

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,8 +1,12 @@
 <?php
 
-include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'Browser.php';
-include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'Language.php';
-include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'OS.php';
+//include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'Browser.php';
+//include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'Language.php';
+//include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'OS.php';
+
+//include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'pua'.DS.'lib'.DS.'phpUserAgent.php';
+//include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'pua'.DS.'lib'.DS.'phpUserAgentStringParser.php';
+include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'RitaUserAgent'.DS.'RitaUserAgent.php';
 
 
 
