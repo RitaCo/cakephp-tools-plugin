8433ac80194c6dcf9d5410f41d347ea803e97a35
zoghal <saleh.souzanchi@gmail.com>
2013-12-27 10:02:36 +0330

    add ip subnet calcutor

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -4,9 +4,9 @@
 //include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'Language.php';
 //include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'OS.php';
 
-//include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'pua'.DS.'lib'.DS.'phpUserAgent.php';
-//include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'pua'.DS.'lib'.DS.'phpUserAgentStringParser.php';
-include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'RitaUserAgent'.DS.'RitaUserAgent.php';
+include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'pua'.DS.'lib'.DS.'phpUserAgent.php';
+include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'pua'.DS.'lib'.DS.'phpUserAgentStringParser.php';
+//include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'RitaUserAgent'.DS.'RitaUserAgent.php';
 
 
 
