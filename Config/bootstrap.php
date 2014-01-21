595addcbaa79fea81aeddac4558b0bc167481b1c
zoghal <saleh.souzanchi@gmail.com>
2014-01-21 18:43:34 +0330

    update

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -4,8 +4,8 @@
 //include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'Language.php';
 //include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'php-browser'.DS.'src'.DS.'Browser'.DS.'OS.php';
 
-include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'pua'.DS.'lib'.DS.'phpUserAgent.php';
-include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'pua'.DS.'lib'.DS.'phpUserAgentStringParser.php';
+include  CakePlugin::path('RitaTools').'Vendor'.DS.'pua'.DS.'lib'.DS.'phpUserAgent.php';
+include  CakePlugin::path('RitaTools').'Vendor'.DS.'pua'.DS.'lib'.DS.'phpUserAgentStringParser.php';
 //include  CakePlugin::path('RitaTools').DS.'Vendor'.DS.'RitaUserAgent'.DS.'RitaUserAgent.php';
 
 
