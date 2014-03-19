412a6929182c5bb42b0d660abab7e770eb327506
zoghal <saleh.souzanchi@gmail.com>
2014-03-20 00:22:42 +0330

    add assets config

---
+++ Config/assets.php
@@ -0,0 +1,43 @@
+<?php
+return[
+	'admin' =>  [
+		'script' => [	
+			'lt' => [
+				'RitaTools.jquery.noty.packaged.min',
+				'RitaTools.RitaNotyTheme',
+				'RitaTools.RitaTools',
+			],
+			'lb' => [
+
+			],
+			'ct' => [],
+			'cb' => [
+				
+			]
+		],
+		'style' => [	
+			'lt' => [
+
+			],
+			'lb' => [],
+			'ct' => [],
+			'cb' => []
+		]
+	],
+	'front' => [
+		'script' => [	
+			'lt' => [],
+			'lb' => [],
+			'ct' => [],
+			'cb' => []
+		],
+		'style' => [	
+			'lt' => [],
+			'lb' => [],
+			'ct' => [],
+			'cb' => []
+		]
+	
+	]
+
+];
