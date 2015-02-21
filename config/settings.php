6e3a548830c5f64ed953b7ce98baf963587ff321
zoghal <saleh.souzanchi@gmail.com>
2015-02-21 16:08:49 +0330

    up

--- config/settings.php
+++ config/settings.php
@@ -3,7 +3,8 @@
 return [
     'css' => [
         'base' => [
-            'Rita/Tools.cake.css'
+            'Rita/Tools.cake.css',
+            'Rita/Tools.icons/rep.css'
         ],
         'admin' => [],
         'front' => []
