4efc7f8941012583197c132bbd2e8924251079bc
zoghal <saleh.souzanchi@gmail.com>
2015-03-22 09:47:32 +0430

    fix indent

--- src/Controller/Component/FlashComponent.php
+++ src/Controller/Component/FlashComponent.php
@@ -8,31 +8,39 @@ use Cake\Utility\Inflector;
 
 class FlashComponent extends \Cake\Controller\Component\FlashComponent
 {
-    
-        public function set($message, array $options = [])
-        {
-        $options += $this->config();
 
+    
+    /**
+     * FlashComponent::set()
+     * 
+     * @param mixed $message
+     * @param mixed $options
+     * @return void
+     */
+    public function set($message, array $options = [])
+    {
+        $options += $this->config();
+    
         if ($message instanceof \Exception) {
             $options['params'] += ['code' => $message->getCode()];
             $message = $message->getMessage();
         }
-
+    
         list($plugin, $element) = pluginSplit($options['element']);
-
+    
         if ($plugin) {
             $options['element'] = $plugin . '.Flash/' . $element;
         } else {
             $options['element'] = 'Flash/' . $element;
         }
-        
+    
         $session = $this->_session->read('Flash.' . $options['key']);
         $session[] = [
-        'message' => $message,
-        'key' => $options['key'],
-        'element' => $options['element'],
-        'params' => $options['params']
+            'message' => $message,
+            'key' => $options['key'],
+            'element' => $options['element'],
+            'params' => $options['params']
         ];
         $this->_session->write('Flash.' . $options['key'], $session);
-        }
+    }
 }
