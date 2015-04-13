e7a2234858edadb8a0776ddbb832a44b44a43b0e
zoghal <saleh.souzanchi@gmail.com>
2015-04-13 20:23:42 +0430

    back cakecss

--- config/assets.php
+++ config/assets.php
@@ -3,7 +3,8 @@
 return [
     'css' => [
         'base' => [
-            'Rita/Tools.icons/rep.css'
+            'Rita/Tools.icons/rep.css',
+            'Rita/Tools.cake.css'
         ],
         'admin' => [],
         'front' => []
